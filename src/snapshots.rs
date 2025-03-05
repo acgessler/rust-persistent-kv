9316568d4b2a923f6e0fb422bf3c73eae4172101
Alexander Gessler <alexander.gessler@gmx.net>
2025-03-05 09:00:23 +0100

    Snapshots: fix read routine to handle very small messages. This is messier than I'd like it to be, but I could not find a less verbose way to correctly handle proto length delimiters.

--- src/snapshots.rs
+++ src/snapshots.rs
@@ -1,8 +1,8 @@
 use prost::Message;
 use std::fs::File;
 use std::fs::OpenOptions;
-use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Read;
 use std::io::Write;
 use std::path::Path;
 use std::sync::Arc;
@@ -43,7 +43,6 @@ impl Drop for SnapshotAppendOp {
 }
 
 impl SnapshotWriter {
-    
     thread_local! {
         static BUFFER: Vec<u8> = const { Vec::new() };
         static ENTRY: SnapshotEntry = SnapshotEntry::default();
@@ -105,23 +104,55 @@ impl SnapshotReader {
         where F: FnMut(&SnapshotEntry)
     {
         // TODO(acgessler): Deal with broken/partly written entries.
-        const CAP: usize = 1 << 16;
-        let mut reader = BufReader::with_capacity(CAP, &self.file);
+        const BUFFER_CAPACITY: usize = 1 << 16;
+        const LENGTH_DELIMITER_LENGTH: usize = 10;
+
+        let mut reader = BufReader::with_capacity(BUFFER_CAPACITY, &self.file);
         let mut entry = SnapshotEntry::default();
+        let mut buf: Vec<u8> = Vec::new();
 
         loop {
-            let length = {
-                let mut buffer = reader.fill_buf()?;
-                if buffer.is_empty() {
-                    break;
-                }
-                let len = buffer.len();
-                entry.clear();
-                entry.merge_length_delimited(&mut buffer)?;
-                callback(&entry);
-                len - buffer.len()
-            };
-            reader.consume(length);
+            // Read up to 10 bytes to decode the length delimiter (varint).
+            buf.clear();
+            let bytes_already_read = reader
+                .by_ref()
+                .take(LENGTH_DELIMITER_LENGTH as u64)
+                .read_to_end(&mut buf)?;
+            if bytes_already_read == 0 {
+                break;
+            }
+            let mut prefix_buf = &buf[..bytes_already_read];
+            let message_length = prost::encoding::decode_varint(&mut prefix_buf)? as usize;
+            // SAFETY: prefix_buf was advanced from the start of buf, so the offset is valid.
+            let bytes_consumed_by_length_delim = (unsafe {
+                prefix_buf.as_ptr().offset_from(buf[..].as_ptr())
+            }) as usize;
+
+            // Keep reading until we have the full message in buf. For very small messages
+            // we have to backtrack, as the delimiter read may have read too much.
+            let message_bytes_read = bytes_already_read - bytes_consumed_by_length_delim;
+            if message_length > message_bytes_read {
+                reader
+                    .by_ref()
+                    .take((message_length - message_bytes_read) as u64)
+                    .read_to_end(&mut buf)?;
+            } else if message_length + bytes_consumed_by_length_delim < bytes_already_read {
+                reader.seek_relative(
+                    (message_length as i64) +
+                        (bytes_consumed_by_length_delim as i64) -
+                        (bytes_already_read as i64)
+                )?;
+            }
+
+            entry.clear();
+            entry.merge(
+                &buf
+                    [
+                        bytes_consumed_by_length_delim..bytes_consumed_by_length_delim +
+                            (message_length as usize)
+                    ]
+            )?;
+            callback(&entry);
         }
         Ok(())
     }
@@ -143,21 +174,38 @@ mod tests {
     #[test]
     fn basic_io() {
         let tmp_file = NamedTempFile::new().unwrap();
-        {
-            let mut writer = SnapshotWriter::new(tmp_file.path(), false, SyncMode::SyncEveryWrite);
-            writer.append_entry(b"foo", Some(b"1")).unwrap();
-            writer.append_entry(b"bar", None).unwrap();
-        }
-        {
-            let mut reader = SnapshotReader::new(tmp_file.path());
-            let entries = reader.read_entries_to_vec().unwrap();
+        let mut writer = SnapshotWriter::new(tmp_file.path(), false, SyncMode::SyncEveryWrite);
+        writer.append_entry(b"foo", Some(b"1")).unwrap();
+        writer.append_entry(b"bar", None).unwrap();
+        writer.append_entry(b"baz", Some(b"2")).unwrap();
+        drop(writer);
 
-            assert_eq!(entries.len(), 2);
-            assert_eq!(entries[0].key, b"foo");
-            assert_eq!(entries[0].value, b"1");
-            assert_eq!(entries[1].key, b"bar");
-            assert_eq!(entries[1].value, b"");
-        }
+        let mut reader = SnapshotReader::new(tmp_file.path());
+        let entries = reader.read_entries_to_vec().unwrap();
+        assert_eq!(entries.len(), 3);
+        assert_eq!(entries[0].key, b"foo");
+        assert_eq!(entries[0].value, b"1");
+        assert_eq!(entries[1].key, b"bar");
+        assert_eq!(entries[1].value, b"");
+        assert_eq!(entries[2].key, b"baz");
+        assert_eq!(entries[2].value, b"2");
     }
-    // TODO: large data size
+
+    #[test]
+    fn basic_large_data() {
+        let tmp_file = NamedTempFile::new().unwrap();
+        let mut writer = SnapshotWriter::new(tmp_file.path(), false, SyncMode::SyncEveryWrite);
+        let very_large_data = b"1".repeat(1000000);
+        writer.append_entry(b"foo", Some(&very_large_data)).unwrap();
+        writer.append_entry(b"bar", Some(&very_large_data)).unwrap();
+        drop(writer);
+
+        let mut reader = SnapshotReader::new(tmp_file.path());
+        let entries = reader.read_entries_to_vec().unwrap();
+        assert_eq!(entries.len(), 2);
+        assert_eq!(entries[0].key, b"foo");
+        assert_eq!(entries[0].value, very_large_data);
+        assert_eq!(entries[1].key, b"bar");
+        assert_eq!(entries[1].value, very_large_data);
+    }
 }
